b72ff907b79f66d546bcaf023469b6c3732af7f8
harrison_hunter <harrison@hunterhayven.com>
2020-01-12 01:53:27 -0600

    Added moving left and right plus number conversion

--- front_end/js/normal.js
+++ front_end/js/normal.js
@@ -1,3 +1,51 @@
+// Converting words to digits acquired from
+// https://stackoverflow.com/questions/11980087/javascript-words-to-numbers
+var numbers = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    'ten': 10,
+    'eleven': 11,
+    'twelve': 12,
+    'thirteen': 13,
+    'fourteen': 14,
+    'fifteen': 15,
+    'sixteen': 16,
+    'seventeen': 17,
+    'eighteen': 18,
+    'nineteen': 19,
+    'twenty': 20,
+    'thirty': 30,
+    'forty': 40,
+    'fifty': 50,
+    'sixty': 60,
+    'seventy': 70,
+    'eighty': 80,
+    'ninety': 90
+};
+
+function text2num(s) {
+    var out = [];
+
+    var a = s.toString().split(/[\s-]+/);
+    a.forEach(function(word) {
+        if (word in numbers) {
+            out.push(numbers[word]);
+        }
+        else { out.push(word) }
+    });
+    return out.join(" ");
+}
+
+
+
 functions = {
     "go to ending": function() {editor.selection.selectLineEnd()},
     "go to start": function() {editor.selection.moveCursorFileStart()},
@@ -44,6 +92,17 @@ commands = {
         editor.navigateFileEnd();
     },
 
+    "MoveLeftX": function(entities) {
+        var number = parseInt(entities[entities.length-1].entity);
+
+        editor.selection.moveCursorBy(0, -1*number);
+    },
+    "MoveRightX": function(entities) {
+        var number = parseInt(entities[entities.length - 1].entity);
+
+        editor.selection.moveCursorBy(0, number);
+    },
+
 };
 
 
@@ -68,12 +127,12 @@ processLuis = function (response) {
 
 normalFinal = function(command, editor) {
     command = command.toLowerCase();
-    console.log(command);
+    console.log(text2num(command));
 
     command = command.trim()
 
     // Get luis response
-    translateCommand(command, processLuis);
+    translateCommand(text2num(command), processLuis);
 
     session = editor.getSession();
 
